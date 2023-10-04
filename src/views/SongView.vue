f9bae885ce5630317423e99175470f5c9f8071dd
HBazai <bazaee.hassan@gmail.com>
2023-10-05 00:40:12 +0330

    playing music

--- src/views/SongView.vue
+++ src/views/SongView.vue
@@ -8,6 +8,7 @@
     <div class="container mx-auto flex items-center">
       <!-- Play/Pause Button -->
       <button
+        @click.prevent="newSong(song)"
         type="button"
         class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
       >
@@ -25,7 +26,7 @@
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <!-- Comment Count -->
-        <span class="card-title">Comments (15)</span>
+        <span class="card-title">Comments ({{ song.comment_count }})</span>
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
@@ -54,6 +55,7 @@
         </vee-form>
         <!-- Sort Comments -->
         <select
+          v-model="sort"
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
         >
           <option value="1">Latest</option>
@@ -64,7 +66,11 @@
   </section>
   <!-- Comments -->
   <ul class="container mx-auto">
-    <li class="p-6 bg-gray-50 border border-gray-200" v-for="comment in comments" :key="comment.id">
+    <li
+      class="p-6 bg-gray-50 border border-gray-200"
+      v-for="comment in sortedComments"
+      :key="comment.id"
+    >
       <!-- Comment Author -->
       <div class="mb-5">
         <div class="font-bold">{{ comment.name }}</div>
@@ -81,8 +87,9 @@
 <script>
 import { songsCollection, auth, commentsCollection } from '../includes/firebase'
 import { ErrorMessage } from 'vee-validate'
-import { mapState } from 'pinia'
+import { mapState, mapActions } from 'pinia'
 import useUserStore from '@/stores/user'
+import usePlayerStore from '@/stores/player'
 
 export default {
   name: 'SongView',
@@ -90,7 +97,15 @@ export default {
     ErrorMessage
   },
   computed: {
-    ...mapState(useUserStore, ['userLoggedIn'])
+    ...mapState(useUserStore, ['userLoggedIn']),
+    sortedComments() {
+      return this.comments.slice().sort((a, b) => {
+        if (this.sort === '1') {
+          return new Date(b.datePosted) - new Date(a.datePosted)
+        }
+        return new Date(a.datePosted) - new Date(b.datePosted)
+      })
+    }
   },
   data() {
     return {
@@ -102,7 +117,8 @@ export default {
       comment_show_alert: false,
       comment_alert_variant: 'bg-blue-500',
       comment_alert_message: 'Please wait! Your comment is being submitted',
-      comments: []
+      comments: [],
+      sort: '1'
     }
   },
   async created() {
@@ -115,6 +131,7 @@ export default {
     this.getComments()
   },
   methods: {
+    ...mapActions(usePlayerStore, ['newSong']),
     async addComment(values, { resetForm }) {
       this.comment_in_submission = true
       this.comment_show_alert = true
@@ -130,6 +147,13 @@ export default {
       }
 
       await commentsCollection.add(comment)
+      this.song.comment_count += 1
+      await songsCollection.doc(this.$route.params.id).update({
+        comment_count: this.song.comment_count
+      })
+
+      this.getComments()
+
       this.comment_in_submission = false
       this.comment_alert_variant = 'bg-green-500'
       this.comment_alert_message = 'Comment added!'
@@ -137,7 +161,7 @@ export default {
     },
 
     async getComments() {
-      const snapshots = await commentsCollection.where('sid' , '==' , this.$route.params.id).get()
+      const snapshots = await commentsCollection.where('sid', '==', this.$route.params.id).get()
 
       this.comments = []
 
